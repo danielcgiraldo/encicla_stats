53f6b46154569c177dc92595777f69f0c318cb77
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-05-01 21:06:34 -0500

    server execute

--- main.py
+++ main.py
@@ -8,7 +8,25 @@ def main():
     :return: None
     """
 
+    temp = {
+        9: -1, # Suramericana
+        46: -1, # Parque de los enamorados
+        93: -1, # Colombia
+        97: -1, # Carlos E. Restrepo
+        98: -1 # Campus Nacional Volador
+    }
+
+    i = 0
+
     while True:
+
+        fecha = str(datetime.now()).split(" ")
+
+        if int(fecha[1][0:2]) < 7 or int(fecha[1][0:2]) >= 19:
+            i += 1
+            print(f"Ciclo {i}")
+            time.sleep(10)
+            continue
 
         url = "https://webapp.metropol.gov.co/wsencicla/api/Disponibilidad/GetDisponibilidadMapas"
 
@@ -16,16 +34,20 @@ def main():
 
         stations = response.json()
         # Fecha en al que se tomo el dato
-        fecha = str(datetime.now()).split(" ")
+        
 
         # TODO: Save the data in a csv for suramericana and volador
         archivo = open("datos.txt", "a") #crear archivo
-        numeros = [9,46,93,97,98] #suramericana,parque de los enamorados, colombia, carlos_E, campus nacional
-        for numero in numeros:
-          archivo.write(str(stations[numero]["name"])+","+ str(stations[numero]["bikes"])+","+fecha[0]+","+fecha[1][0:8]+"\n")
+        
+        for key, value in temp.items():
+            if stations[key]["bikes"] != value:
+                archivo.write(str(stations[key]["name"])+","+ str(stations[key]["bikes"])+","+fecha[0]+","+fecha[1][0:8]+"\n")
+                temp[key] = stations[key]["bikes"]
         archivo.close()
-        # Wait 1 minute before next request
-        time.sleep(60)
+        # Wait 10 seconds before next request
+        i += 1
+        print(f"Ciclo {i}")
+        time.sleep(10)
 
 
 if __name__ == '__main__':
