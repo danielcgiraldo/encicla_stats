37fbee3274997f668eec7aced3ce39b84b53457e
Daniel Castillo Giraldo <dc.giraldocg@hotmail.com>
2023-06-22 01:41:39 -0500

    Nuevos entrega

--- datarizer/time_interval.py
+++ datarizer/time_interval.py
@@ -1,24 +1,27 @@
 import csv
 
-FILE_READ = "Prestamo"
+FILE_READ = "Entrega"
 
 def time_to_seconds(time_str):
     hours, minutes, seconds = map(int, time_str.split(':'))
     return hours * 3600 + minutes * 60 + seconds
 
-with open(f"unal/fixed.csv", 'r') as f:
+with open(f"datarizer/preprocessing/fixed.csv", 'r') as f:
     reader = csv.reader(f)
-    for row in reader:
-        if row[1][:2] not in ["14", "15"] or row[0] not in ["7/6/2023", "8/6/2023", "14/6/2023"] or row[2] != FILE_READ:
+    list_reader = list(reader)
+
+    for i in range(len(list_reader)):
+        print(i)
+        row = list_reader[i]
+        if row[1][:2] not in ["14", "15"] or row[0] != "16/06/2023" or row[2] != FILE_READ:
             continue
-        
-        row_next = next(reader)
+        row_next = list_reader[i + 1]
         while row_next[2] != FILE_READ:
-            row_next = next(reader)
-        time_diff = time_to_seconds(row_next[1]) - time_to_seconds(row[1])
-        if time_diff > 500:
-            print(row)
+            row_next = list_reader[i + 1]
+            i += 1
+        if row_next[0] != "16/06/2023":
             continue
-        with open(f"unal/{FILE_READ}.csv", 'a', newline='') as f:
+        time_diff = time_to_seconds(row_next[1]) - time_to_seconds(row[1])
+        with open(f"data/{FILE_READ}.csv", 'a', newline='') as f:
             writer = csv.writer(f)
             writer.writerow([time_diff])
